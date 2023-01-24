ab58bdd8602cdc985f3582c2313fa087707ad1af
John <jrc2855@gmail.com>
2023-01-24 00:13:52 +0000

    Created books route/ REST Client

--- server.js
+++ server.js
@@ -6,16 +6,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const Book = require(/models/books.js)
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3002;
-
-//-----ENDPOINTS-----//
-app.get('/test', (request, response) => {
-  response.status(200).send('test request received')
-})
 
 mongoose.connect(process.env.DB_URL);
 
@@ -24,12 +19,29 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
   console.log('Mongoose is connected');
 });
+//-----ENDPOINTS-----//
+app.get('/test', (request, response) => {
+  response.status(200).send('test request received')
+})
 
-app.get('*', (request,response)=> {
+app.get('/books', getBooks);
+
+async function getBooks(request, response, next) {
+  try {
+    let allBooks = await books.find({});
+
+    response.status(200).send(allBooks);
+  } catch {
+    console.log(error.Message);
+    next(error);
+  }
+}
+
+app.get('*', (request, response) => {
   response.status(404).send('Not Available');
 });
 
-app.use((error, request, response, next) =>{
+app.use((error, request, response, next) => {
   response.status(500).send(Error.Message);
 });
 
